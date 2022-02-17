dee16c1206e46a7692cb8c5aa2f9d263275d906f
Alumetriz <fanmax0808@gmail.com>
2022-02-17 20:24:34 +0200

    add: finish layout site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img src="./images/svg/arrow-left.svg" alt="" class="arrow arrow-left">',
+        nextArrow: '<img src="./images/svg/arrow-right.svg" alt="" class="arrow arrow-right">',
+    });
 });
